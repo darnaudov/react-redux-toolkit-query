afc3933f54c4b2e65f0627981f774ab4edf869d7
Daniel Arnaudov <daniel.arnaudov@gmail.com>
2022-09-21 13:46:54 +0300

    Extract mock data in separate file

---
+++ src/mocks/mockData.ts
@@ -0,0 +1,29 @@
+export const mockProducts = [
+  {
+    id: 1,
+    name: 'MacBook',
+    price: 1400,
+    seller: {
+      id: 1,
+      name: 'George',
+    },
+  },
+  {
+    id: 2,
+    name: 'Old Car',
+    price: 2400,
+    seller: {
+      id: 2,
+      name: 'Susan',
+    },
+  },
+  {
+    id: 3,
+    name: 'W Shoes',
+    price: 999,
+    seller: {
+      id: 1,
+      name: 'George',
+    },
+  },
+];
