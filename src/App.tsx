556cdbc13cf28b1d93d10e5f798c1d2781fad1ee
Daniel Arnaudov <daniel.arnaudov@gmail.com>
2022-09-30 15:09:43 +0300

    Add redux-persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { store, persistor } from 'redux/store';
 import router from 'pages/router';
 import { RouterProvider } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
   return (
     <Provider store={store}>
-      <RouterProvider router={router}></RouterProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router}></RouterProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,10 +5,26 @@ import {
   PreloadedState,
   combineReducers,
 } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from 'redux/slices/user';
 import productsReducer from 'redux/slices/products';
 import cartItemsReducer from 'redux/slices/cartItems';
 import sellersReducer from 'redux/slices/sellers';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
 const rootReducer = combineReducers({
   user: userReducer,
@@ -17,14 +33,23 @@ const rootReducer = combineReducers({
   sellers: sellersReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
   return configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
   });
 };
 
 export const store = setupStore();
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
