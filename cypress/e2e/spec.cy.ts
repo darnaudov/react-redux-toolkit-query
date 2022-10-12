374b22b807b92e482b803d3d1f0dcdf1ae99ae26
Daniel Arnaudov <daniel.arnaudov@gmail.com>
2022-10-12 18:03:17 +0300

    Update cypress tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,61 +2,68 @@
 
 describe('Product cart', () => {
   it('should be able to add and remove products from products list and cart', () => {
-    cy.fixture('products').then((products) => {
-      cy.intercept(
-        {
-          method: 'GET',
-          url: '/products',
-        },
-        products
-      ).as('getProducts');
+    cy.fixture('user').then((user) => {
+      cy.fixture('products').then((products) => {
+        cy.intercept(
+          {
+            method: 'GET',
+            url: '/products',
+          },
+          products
+        ).as('getProducts');
 
-      cy.visit('/');
-      cy.contains(/home/i);
-      cy.contains(/wellcome user/i);
-      cy.contains(/products/i).click();
-      cy.url().should('include', '/products');
+        cy.visit('/');
+        cy.url().should('include', '/login');
+        cy.contains(/email/i).type(user.email);
+        cy.contains(/password/i).type(user.password);
+        cy.contains(/login/i).click();
 
-      let totalPrice = 0;
-      for (let product of products) {
-        cy.contains(`${product.name} ${product.price}`)
-          .contains(/add to cart/i)
-          .click();
+        cy.contains(/home/i);
+        cy.contains(new RegExp(`wellcome ${user.email}`, 'i'));
+        cy.contains(/products/i).click();
+        cy.url().should('include', '/products');
 
-        totalPrice += product.price;
-      }
+        let totalPrice = 0;
+        for (let product of products) {
+          cy.contains(`${product.name} ${product.price}`)
+            .contains(/add to cart/i)
+            .click();
 
-      cy.contains(/cart \(3\)/i).click();
-      cy.url().should('include', '/cart');
-      cy.contains(`Total Price: $${totalPrice}`);
+          totalPrice += product.price;
+        }
 
-      for (let product of products) {
-        cy.contains(`${product.name} ${product.price} x 1`)
-          .contains(/\+/i)
-          .click()
-          .click();
-      }
+        cy.contains(/cart \(3\)/i).click();
+        cy.url().should('include', '/cart');
+        cy.contains(`Total Price: $${totalPrice}`);
 
-      cy.contains(`Total Price: $${totalPrice * 3}`);
+        for (let product of products) {
+          cy.contains(`${product.name} ${product.price} x 1`)
+            .contains(/\+/i)
+            .click()
+            .click();
+        }
 
-      for (let product of products) {
-        cy.contains(`${product.name} ${product.price} x 3`)
-          .contains(/-/i)
-          .click();
-      }
+        cy.contains(`Total Price: $${totalPrice * 3}`);
 
-      cy.contains(`Total Price: $${totalPrice * 2}`);
+        for (let product of products) {
+          cy.contains(`${product.name} ${product.price} x 3`)
+            .contains(/-/i)
+            .click();
+        }
 
-      for (let product of products) {
-        cy.contains(`${product.name} ${product.price} x 2`);
-      }
+        cy.contains(`Total Price: $${totalPrice * 2}`);
 
-      cy.contains(/clear cart/i).click();
-      cy.contains('Total Price: $0');
+        for (let product of products) {
+          cy.contains(`${product.name} ${product.price} x 2`);
+        }
 
-      for (let product of products) {
-        cy.contains(`${product.name} ${product.price}`).should('not.exist');
-      }
+        cy.contains(/clear cart/i).click();
+        cy.contains('Total Price: $0');
+
+        for (let product of products) {
+          cy.contains(`${product.name} ${product.price}`).should('not.exist');
+        }
+      });
     });
   });
 });
